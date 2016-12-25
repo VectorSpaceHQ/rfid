75c9627d0e2dc572cd7ca3ddb0ec6a1a61f1579f
Adam S <spontarelliam@gmail.com>
2016-12-24 22:26:39 -0500

    changed bitbang example to match original dumpinfo pinout for easier debugging.

--- examples/DumpInfoBitBang/DumpInfoBitBang.ino
+++ examples/DumpInfoBitBang/DumpInfoBitBang.ino
@@ -3,7 +3,7 @@
  * Example sketch/program showing how to use multiple card readers
  * --------------------------------------------------------------------------------------------------------------------
  * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
- * 
+ *
  */
 
 #include <MFRC522.h>
@@ -11,24 +11,23 @@
 #define RST_1_PIN   2
 #define SS_1_PIN    3
 #define MISO_1_PIN  4
-#define SCK_1_PIN   11
-#define MOSI_1_PIN  12
 
 #define RST_2_PIN   5
 #define SS_2_PIN    6
 #define MISO_2_PIN  7
-#define SCK_2_PIN   11
-#define MOSI_2_PIN  12
 
-#define RST_3_PIN   8
-#define SS_3_PIN    9
-#define MISO_3_PIN  10
-#define SCK_3_PIN   11
-#define MOSI_3_PIN  12
+#define RST_3_PIN   9
+#define SS_3_PIN    10
+#define MISO_3_PIN  12
 
-MFRC522 mfrc522_1(SS_1_PIN, RST_1_PIN, MISO_1_PIN, MOSI_1_PIN, SCK_1_PIN);  // Create MFRC522 instance
-MFRC522 mfrc522_2(SS_2_PIN, RST_2_PIN, MISO_2_PIN, MOSI_2_PIN, SCK_2_PIN);  // Create MFRC522 instance
-MFRC522 mfrc522_3(SS_3_PIN, RST_3_PIN, MISO_3_PIN, MOSI_3_PIN, SCK_3_PIN);  // Create MFRC522 instance
+#define SCK_PIN   13
+#define MOSI_PIN  11
+
+
+MFRC522 mfrc522_1(SS_1_PIN, RST_1_PIN, MISO_1_PIN, MOSI_PIN, SCK_PIN);  // Create MFRC522 instance
+MFRC522 mfrc522_2(SS_2_PIN, RST_2_PIN, MISO_2_PIN, MOSI_PIN, SCK_PIN);  // Create MFRC522 instance
+MFRC522 mfrc522_3(SS_3_PIN, RST_3_PIN, MISO_3_PIN, MOSI_PIN, SCK_PIN);  // Create MFRC522 instance
+/* MFRC522 mfrc522_3(SS_3_PIN, RST_3_PIN);  // Create MFRC522 instance */
 
 void setup() {
     Serial.begin(9600);     // Initialize serial communications with the PC
@@ -52,6 +51,7 @@ void setup() {
 void loop() {
 
     if (mfrc522_1.PICC_IsNewCardPresent()) {
+        Serial.println("card present");
         if (mfrc522_1.PICC_ReadCardSerial()) {
             Serial.println(F("Card on Reader 1"));
             mfrc522_1.PICC_DumpToSerial(&(mfrc522_1.uid));
@@ -66,6 +66,7 @@ void loop() {
     }
 
     if (mfrc522_3.PICC_IsNewCardPresent()) {
+        Serial.println("card3 present");
         if (mfrc522_3.PICC_ReadCardSerial()) {
             Serial.println(F("Card on Reader 3"));
             mfrc522_3.PICC_DumpToSerial(&(mfrc522_3.uid));
